41e3936a86dccdde080d19aee4b2e6a0c8194a5b
Paul Anderson <pauleanderson@gmail.com>
2022-07-08 16:58:51 -0700

    update

--- labs/Lab4_helper.py
+++ labs/Lab4_helper.py
@@ -37,7 +37,7 @@ def print_tree(tree):
     mytree = copy.deepcopy(tree)
     def fix_keys(tree):
         if type(tree) != dict:
-            return int(tree)
+            return tree #int(tree)
         new_tree = {}
         for key in list(tree.keys()):
             if type(key) == np.int64:
@@ -72,57 +72,13 @@ def split_col(x, y):
     return g, c
 
 def select_split2(X,y):
-    col = None
-    gr = float("-inf")
-    best_split = None
-    for c in X.columns:
-        if X[c].dtype in ['int64','float64']:
-            
-            save_x = X[c].copy()
-            x2 = list(X[c].copy())
-            x2.sort()
-            
-            splits = []
-            for i in range(0, len(x2)-1):
-                splits.append((x2[i] + x2[i+1]) / 2)
-                
-            for split in splits:
-                save_x = save_x.apply(lambda x: 'True' if x < split else 'False')
-                g = gain_ratio(y, save_x)
-                if g > gr:
-                    gr, col, best_split = g, c, split
-                save_x = X[c].copy()
-        else:
-            g = gain_ratio(y, X[c])
-            if g > gr:
-                gr, col, best_split = g, c, None
-    newname = col
-    if best_split:
-        newname = '%s<%.2f'%(col, best_split)
-        X.rename(columns={col:newname}, inplace=True)
-        X[newname] = X[newname].apply(lambda x: "True" if x < best_split else "False")
+    newname, gr = None, None
+    # Your solution here
     return newname ,gr
 
 def make_tree2(X,y,min_split_count=5):
-    X = X.copy()
     tree = {}
-    if len(X.columns) == 0:
-        return y.value_counts().idxmax()
-    if len(y.unique()) == 1:
-        return y.unique()[0]
-    if len(X) < min_split_count:
-        return y.value_counts().idxmax()
-    col, gr = select_split2(X, y)
-    if gr < 0.001:
-        return y.value_counts().idxmax()
-    tree[col] = {}
-    for ux in X[col].unique():
-        tree[col][ux] = {}
-        y2 = y[X[col] == ux]
-        X2 = X.copy()
-        X2 = X2[X2[col] == ux]
-        X2 = X2.drop(col, axis=1)
-        tree[col][ux] = make_tree2(X2, y2, min_split_count)
+    # Your solution here
     return tree
 
 def make_prediction(rules,x,default):
